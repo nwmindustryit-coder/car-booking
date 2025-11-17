dd980fe8faf6d9e1abc038d28c080c7fd5f76893
nwmindustryit-coder <nwmindustryit@gmail.com>
2025-11-17 08:45:18 +0700

    Add feature line OA

--- line/richmenu/upload-image.ts
+++ line/richmenu/upload-image.ts
@@ -1,7 +1,8 @@
 import fs from "fs";
-import { lineClient } from "../client";
+import { getLineClient  } from "../client";
 
 export async function uploadImage() {
+  const lineClient = getLineClient();
   const richMenuId = process.env.LINE_RICH_MENU_ID!; 
   const buffer = fs.readFileSync("./public/richmenu.png");
 
